b2f2e93f7631d1f1f15555cdb349fcdd3bec6354
KeyDiS <KeyDiS-FBD@github.com>
2019-10-22 15:19:04 +0600

    Last fix

--- first_step/first_module.py
+++ first_step/first_module.py
@@ -4,8 +4,8 @@ from urllib.parse import quote
 
 name = quote(input())
 html = urlopen("https://ru.wikipedia.org/wiki/" + name)
-bsObj = BeautifulSoup(html.read(), features = 'lxml');
-for link in bsObj.find_all('a'):
+doc = BeautifulSoup(html.read(), features = 'lxml');
+for link in doc.find_all('a'):
     word = str(link.get('href'))
     if(word.find('https', 0, -1) != -1):
             print(word)

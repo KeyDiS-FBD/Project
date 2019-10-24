a66c28cc68ed02410219c366bfb174b2155d5aa6
KeyDiS <KeyDiS-FBD@github.com>
2019-10-24 10:20:19 +0600

    Fixed the problem of outputting unreadable letters

--- first_step/first_module.py
+++ first_step/first_module.py
@@ -1,6 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from urllib.parse import quote
+from urllib.parse import quote, unquote
+
+#def print_another_letters(str word)
+
 
 name = quote(input())
 html = urlopen("https://ru.wikipedia.org/wiki/" + name)
@@ -8,6 +11,8 @@ doc = BeautifulSoup(html.read(), features = 'lxml');
 for link in doc.find_all('a'):
     word = str(link.get('href'))
     if(word.find('wiki', 1, 5) != -1):
-        print('https://ru.wikipedia.org' + word)
-    elif(word.find('https://ru.wikipedia', 0, -1) != -1):
+        word = unquote(unquote('https://ru.wikipedia.org' + word))
+        print(word)
+    elif(word.find('https://ru.wikipedia.org', 0, -1) != -1):
+        word = unquote(unquote(word))
         print(word)

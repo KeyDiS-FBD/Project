b45678f53b2abd65a79dd54d15f664af3dc4535f
KeyDiS <KeyDiS-FBD@github.com>
2019-10-24 10:22:11 +0600

    Fixed the problem of outputting unreadable letters

--- first_step/first_module.py
+++ first_step/first_module.py
@@ -11,8 +11,8 @@ doc = BeautifulSoup(html.read(), features = 'lxml');
 for link in doc.find_all('a'):
     word = str(link.get('href'))
     if(word.find('wiki', 1, 5) != -1):
-        word = unquote(unquote('https://ru.wikipedia.org' + word))
+        word = unquote('https://ru.wikipedia.org' + word)
         print(word)
     elif(word.find('https://ru.wikipedia.org', 0, -1) != -1):
-        word = unquote(unquote(word))
+        word = unquote(word)
         print(word)

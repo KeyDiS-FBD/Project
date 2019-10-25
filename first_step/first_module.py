d6e6a344e21495800f97dc77c8a0c46d293d6cc1
KeyDiS <KeyDiS-FBD@github.com>
2019-10-25 19:24:43 +0600

    Small fixes

--- first_step/first_module.py
+++ first_step/first_module.py
@@ -2,46 +2,43 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.parse import quote, unquote
 
-def push_in_dict(exception, url):
-    pass
-
-def check_n_print(url, exception):
+def check_from_excptn(url, stat_excptn):
     url = unquote(url)
-    for i in exception.keys():
-        if(url.find(exception[i], 0, -1) != -1):
-            return
+    
+    for excptn in stat_excptn.values():
+        if(url.find(excptn, 0, -1) != -1):
+            return 0
+    
     if(url.find('wiki', 1, 5) != -1):
         url = 'https://ru.wikipedia.org' + url
-        
     if(url.find('https://ru.wikipedia.org', 0, -1) != -1):
         print(url)
-
         
 
 #Exception from rules
-exception = {0: 'Википедия',1: 'Служебная',2: 'Категория', 3: 'Портал',4: 'Special',5: 'index'}
-exception[6] = 'Шаблон'
-exception[7] = 'Обсуждение'
-exception[8] = 'Файл'
-exception[9] = 'Проект'
+stat_excptn = {0: 'Википедия',1: 'Служебная',2: 'Категория', 3: 'Портал',4: 'Special',5: 'index'}
+stat_excptn[6] = 'Шаблон'
+stat_excptn[7] = 'Обсуждение'
+stat_excptn[8] = 'Файл'
+stat_excptn[9] = 'Проект'
 
 
-name = input()
+page = input()
 step = 1
 
-if(name == 'Старт'):
+if(page == 'Старт'):
+    page = input()
     print('Step:', step)
     step += 1
-    name = input()
-    while name != 'Выход':
-        name = quote(name)
-        html = urlopen("https://ru.wikipedia.org/wiki/" + name)
+
+    while page != 'Выход':
+        page =  ' https://ru.wikipedia.org/wiki/' + quote(page)
+        html = urlopen(page)
         doc = BeautifulSoup(html.read(), features = 'lxml');
         for link in doc.find_all('a'):
             url = str(link.get('href'))
-            check_n_print(url, exception)
-        print('Step:', step)
-        step += 1
-        name = input()
-
-
+            check_from_excptn(url, stat_excptn)
+        page = input()
+        if(page != 'Выход'):
+            print('Step:', step)
+            step += 1
